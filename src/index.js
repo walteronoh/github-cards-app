09c7003b75ed1d235800045ee6f522473a6fd215
Walter <walterkiprono81@gmail.com>
2021-05-20 09:58:41 +0300

    Added Bootstrap styling and ajax changed from axios to window fetch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-
-
-//const gitData=[{name:"Walteronoh",avatar_url:"https://avatars.githubusercontent.com/u/48877319?v=4"},
-//{name:"Susan",avatar_url:	"https://avatars.githubusercontent.com/u/2305795?v=4"}];
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const CardList=(props)=>(
   <div>
-    <h1>Github Cards App</h1>
+    <h3 className="display-title">The Github User Credentials Will Display Here</h3>
     {props.profiles.map(profile=><Cards key={profile.id} {...profile}/>)}
   </div>
 )
@@ -16,9 +13,12 @@ const CardList=(props)=>(
 class Cards extends React.Component{
   render(){
     return(
-    <div>
-      <p>{this.props.name}</p>
-      <img alt="github profile" src={this.props.avatar_url}/>
+    <div className="profile-style">
+      <img className="img-responsive col-md-3" alt="github profile" src={this.props.avatar_url}/>
+      <div className="user-info">
+        <div>{this.props.name}</div>
+        <div>{this.props.company}</div>      
+      </div>
     </div>
     )
   }
@@ -31,17 +31,20 @@ class Form extends React.Component{
       username:''
     }
   }
-  handleSubmit=async(e)=>{
+  handleSubmit=(e)=>{
     e.preventDefault();
-    const resp= await axios.get(`https://api.github.com/users/${this.state.username}`);
-    this.props.onSubmit(resp.data);
+    fetch(`https://api.github.com/users/${this.state.username}`).then(resp=>{
+      resp.json().then(data=>{
+        this.props.onSubmit(data);
+      })
+    })
   }
   render(){
     return(
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input onChange={event=>this.setState({username:event.target.value})} placeholder="Enter Name" type="text"/><br/>
-          <button type="submit">Add Card</button>
+          <input className="form-control" onChange={event=>this.setState({username:event.target.value})} placeholder="Enter Github Username" type="text"/><br/>
+          <button className="form-control btn btn-info" type="submit">Add Card</button>
         </form>
       </div>
     )
@@ -63,7 +66,8 @@ class App extends React.Component{
   }
   render(){
     return(
-      <div>
+      <div className="container">
+        <h3 className="display-title">The Github Cards App</h3>
         <Form onSubmit={this.addNewProfile}/>
         <CardList profiles={this.state.profile}/>
       </div>
